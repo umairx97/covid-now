45dec2395bba244fcecdc2bd6bc1275967369ddc
umairx97 <umair.bajwa@hotmail.com>
2020-09-27 06:27:18 +0500

    feat: update external api to rapid-api

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const yargs = require('yargs')
 const { promisify } = require('util')
 const chalk = require('chalk')
 const request = promisify(require('request').get)
-const SUBSCRIPTION_KEY = 'cf146759ec47426c903aa5291eafd41c'
-const BASE_URL = 'https://api.smartable.ai'
+const SUBSCRIPTION_KEY = '13faaf98c4msh60d857634ffa70dp1bc433jsn995dbaba52d6'
+const BASE_URL = 'https://coronavirus-smartable.p.rapidapi.com'
 
 var argv = yargs
   .usage('Usage: -c PK')
@@ -19,8 +19,12 @@ var argv = yargs
 const getCovidData = async (countryCode) => {
   try {
     const opts = {
-      url: `${BASE_URL}/coronavirus/stats/${countryCode}`,
-      headers: { 'Subscription-Key': SUBSCRIPTION_KEY }
+      url: `${BASE_URL}/stats/v1/${countryCode}/`,
+      headers: {
+        'x-rapidapi-host': 'coronavirus-smartable.p.rapidapi.com',
+        'x-rapidapi-key': SUBSCRIPTION_KEY,
+        useQueryString: true
+      }
     }
     const data = await request(opts)
     return JSON.parse(data.body)
